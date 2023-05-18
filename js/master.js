918a73d90011aa9b96fc180141d0a18910880315
sohee han <gks0304hsh@naver.com>
2023-05-18 15:53:49 +0900

    0518 15

---
+++ js/master.js
@@ -0,0 +1,36 @@
+/*pc서브메뉴 허버 메서드*/
+$('.pcNav h4:nth-child(5)').hover(function(){
+    $('.pcSubmenu').slideDown();
+},function(){
+    $('.pcSubmenu').slideUp();
+});
+
+//사이드메뉴 나오는 코드
+$('.list').click(function(){
+    $('.sideMenuBox').fadeIn(200);
+    $('.sideMenuContent').animate({
+        right:0+"%"
+    });
+    $("body").addClass('notScroll');
+});
+
+//사이드메뉴 사라지는 코드
+$('.close').click(function(){
+    popclose();
+});
+/*사이드메뉴 사라지는 코드 | 함수 선언*/
+function popclose(){
+    $('.sideMenuContent').animate({
+        right:-100 + '%'
+    },300,function(){
+        $('.sideMenuBox').fadeOut(200);
+    });
+    $("body").removeClass('notScroll');
+}
+
+//사이드메뉴 바깥영역 클릭시 사이드메뉴사라지는 코드
+$(document).click(function (e){
+    if($('.sideMenuBox').is(e.target)){
+        popclose();
+    }
+});
